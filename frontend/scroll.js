3176ee522324a4b27f10e6298c9a8431462efa82
HailNicole <mconstantem1@est.ups.edu.ec>
2024-07-11 00:49:41 -0500

    Commit Final.... espero

---
+++ frontend/scroll.js
@@ -0,0 +1,24 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '85px',
+    duration: 2500,
+    reset: false
+});
+
+sr.reveal('.contenido-platos',{delay:300});
+sr.reveal('.galery',{delay:300});
+sr.reveal('.questions',{delay:300});
+
+sr.reveal('.header-form-container',{delay:300});
+sr.reveal('.container-preguntas',{delay:300});
+sr.reveal('.container-boton',{delay:300});
+
+sr.reveal('.rotate-images',{delay:300});
+sr.reveal('.articulo',{delay:300});
+sr.reveal('.videos-container',{delay:300});
+sr.reveal('.titulo-arte-musical',{delay:300});
+
+sr.reveal('.carousel',{delay:300});
+
+sr.reveal('.images',{delay:300});
+sr.reveal('.header-text',{delay:300});
